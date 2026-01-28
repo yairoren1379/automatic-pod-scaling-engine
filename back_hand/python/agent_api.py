24fbbc443a10a1880266fc55378e1a1cec440907
yairoren1379 <yairoren2312@gmail.com>
2026-01-28 14:03:06 +0200

    added api agent

--- back_hand/python/agent_api.py
+++ back_hand/python/agent_api.py
@@ -8,8 +8,6 @@ class ClusterState(BaseModel):
     cpu_usage: float
     is_crashed: bool
 
-# 2. נקודת הקצה (End Point)
-# לכאן Go ישלח את הנתונים, ומכאן נחזיר לו פקודה
 @app.post("/decide")
 async def get_action(state: ClusterState):
     print(f" Brain received state: {state}")
@@ -17,12 +15,10 @@ async def get_action(state: ClusterState):
     action = "do_nothing"
     
     if state.is_crashed:
-        action = "scale_up" # אם קרס - תוסיף עזרה!
+        action = "scale_up"
         print(" CRASH DETECTED! Ordering Scale Up.")
     elif state.cpu_usage > 80:
         action = "scale_up"
         print(" High Load! Ordering Scale Up.")
     
     return {"action": action}
-
-# להרצה: uvicorn agent_api:app --reload --port 8000
